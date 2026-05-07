a308292c2977d388c3a4fc564d9cc80658c36010
Samuel CARDOSO <scardoso.engr@proton.me>
2026-05-07 11:55:29 +0200

    refactor: remove centralized benchmark orchestration

--- scripts/benchmark/run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-scripts/benchmark/run_core_model_benchmark.sh
-
-if [ "${BENCHMARK_RUN_SERVICE:-false}" = "true" ]; then
-  scripts/benchmark/run_core_service_benchmark.sh
-fi
